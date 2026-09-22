e48f5aca74fe7d0bb11df7a7000e383ae1573609
Carlos Ramos Torres <carlosrt1994@gmail.com>
2026-09-22 16:02:58 +0200

    consultando la base de datos y mostrando la información

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -109,7 +109,12 @@ export const uploadImage = async (req: Request, res: Response) => {
 export const getUserByHandle = async (req: Request, res: Response) => {
     try {
         const { handle } = req.params;
-        console.log(handle);
+        const user = await User.findOne({handle}).select('-_id -__v -email -password')
+        if(!user) {
+            const error = new Error('El usuario no existe')
+            return res.status(404).json({error: error.message})
+        }
+        res.json(user)
     } catch(e) {
         const error = new Error('Hubo un error')
         return res.status(500).json({error: error.message})
